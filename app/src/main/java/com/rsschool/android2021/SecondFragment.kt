8fa55826f4191b9e7f51fe4537bde1a6b3fd4115
AirRio <45617410+AirRio@users.noreply.github.com>
2021-05-31 22:35:58 +0300

    Update SecondFragment.kt

--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,13 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
+interface OpenFirstFragment{
+    fun openFirst(result: Int)
+}
+
 class SecondFragment : Fragment() {
 
+    private var listen: OpenFirstFragment? = null
     private var backButton: Button? = null
     private var result: TextView? = null
 
@@ -21,6 +27,11 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is OpenFirstFragment) listen = context
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         result = view.findViewById(R.id.result)
@@ -29,16 +40,18 @@ class SecondFragment : Fragment() {
         val min = arguments?.getInt(MIN_VALUE_KEY) ?: 0
         val max = arguments?.getInt(MAX_VALUE_KEY) ?: 0
 
-        result?.text = generate(min, max).toString()
+        val randomValue = generate(min, max)
+        result?.text = randomValue.toString()
 
         backButton?.setOnClickListener {
-            // TODO: implement back
+            listen?.openFirst(randomValue)
         }
     }
 
     private fun generate(min: Int, max: Int): Int {
-        // TODO: generate random number
-        return 0
+        return if(min < max) {
+            (min..max).random()
+        } else 0
     }
 
     companion object {
@@ -47,9 +60,9 @@ class SecondFragment : Fragment() {
         fun newInstance(min: Int, max: Int): SecondFragment {
             val fragment = SecondFragment()
             val args = Bundle()
-
-            // TODO: implement adding arguments
-
+            args.putInt(MIN_VALUE_KEY, min)
+            args.putInt(MAX_VALUE_KEY, max)
+            fragment.arguments = args
             return fragment
         }
 
