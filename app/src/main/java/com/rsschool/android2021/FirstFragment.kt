58881a8a19640bf7956cbdbda3c9a1e5325381d9
AirRio <45617410+AirRio@users.noreply.github.com>
2021-05-31 17:39:36 +0300

    Update FirstFragment.kt

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,15 +1,23 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
+
+interface OpenSecondFragment {
+    fun openSecond(min: Int, max: Int)
+}
+
 class FirstFragment : Fragment() {
 
+    private var listen: OpenSecondFragment? = null
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
 
@@ -21,6 +29,11 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is OpenSecondFragment) listen = context
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         previousResult = view.findViewById(R.id.previous_result)
@@ -29,11 +42,19 @@ class FirstFragment : Fragment() {
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
 
-        // TODO: val min = ...
-        // TODO: val max = ...
+        val minView = view.findViewById<EditText>(R.id.min_value)
+        val maxView = view.findViewById<EditText>(R.id.max_value)
 
         generateButton?.setOnClickListener {
-            // TODO: send min and max to the SecondFragment
+            val minValue = minView.text.toString().toIntOrNull()
+            val maxValue = maxView.text.toString().toIntOrNull()
+
+            when {
+                minValue == null -> minView.error = "Min shouldn't be empty or invalid"
+                maxValue == null -> maxView.error = "Max shouldn't be empty or invalid"
+                minValue >= maxValue -> maxView.error = "Max value must be greater then Min"
+                else -> listen?.openSecond(minValue, maxValue)
+            }
         }
     }
 
